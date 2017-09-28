a0d3d198153152f87d39c2cac00675f0c691a5eb
Jonathan <lee.jonathanw@gmail.com>
2017-09-27 23:55:10 -0500

    Added settings.json files and super-admin setup

--- imports/api/users/server/publications.js
+++ imports/api/users/server/publications.js
@@ -2,5 +2,9 @@ import { Meteor } from 'meteor/meteor';
 import { Users } from '../users';
 
 Meteor.publish('currentUser', function() {
-  return Meteor.users.find({_id: this.userId});
+  return Meteor.users.find({_id: this.userId}, {
+    fields:{
+      roles: 1
+    }
+  });
 });
